22d40eaeb91e4255a6326f325d839e72f7462c0b
Ronald Bautista <ronbau21@hotmail.com>
2022-03-29 19:42:35 -0400

    Se incorporó el paquete datos y la clase Departamentos con sus atributos y métodos correspondientes de acuerdo con la base de datos

---
+++ src/Datos/Departamento.java
@@ -0,0 +1,59 @@
+
+package Datos;
+
+/**
+ * @author Ronald Bautista
+ */
+
+public class Departamento {
+    private int idDepartamento;
+    private String nombre;
+    private float superficie;
+    private String imagen;
+
+    public Departamento() {
+    }
+
+    public Departamento(int idDepartamento, String nombre, float superficie, String imagen) {
+        this.idDepartamento = idDepartamento;
+        this.nombre = nombre;
+        this.superficie = superficie;
+        this.imagen = imagen;
+    }
+
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(float superficie) {
+        this.superficie = superficie;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    
+    
+    
+    
+}
