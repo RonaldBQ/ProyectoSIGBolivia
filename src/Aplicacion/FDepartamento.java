8d5bdbed1cba7c992184bc31eb1e2e10eb15ca58
Ronald Bautista <ronbau21@hotmail.com>
2022-03-30 16:08:42 -0400

    Create FDepartamento.java
    Se la clase FDepartamento y agregó el metodo mostrar que devuelve todos los registros de la tabla departamento en un DefaultTableModel

---
+++ src/Aplicacion/FDepartamento.java
@@ -0,0 +1,50 @@
+
+package Aplicacion;
+
+import DBUtils.DBConexion;
+import static DBUtils.DBConexion.conectar;
+import Datos.Departamento;
+import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * @author Ronald Bautista
+ */
+public class FDepartamento {
+    private DBConexion objBaseDatos = new DBConexion();
+    private Connection cn = objBaseDatos.getConection();
+    private String SQL = "";
+    public int totalRegistros;
+    
+    /**
+     * Método que retorna todos los registros de la tabla departamentos en un modelo DefaultTabkeModel
+     * @return modelo
+     */
+    public DefaultTableModel mostrar(){
+        DefaultTableModel modelo;
+        String [] titulos ={"CODIGO","NOMBRE","SUPERFICIE","IMAGEN"};
+        String [] registro = new String[4];
+        totalRegistros = 0;
+        modelo = new DefaultTableModel(null, titulos);
+        SQL = "SELECT * FROM departamento";
+        try {
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(SQL);
+            while(rs.next()){
+                registro[0]=rs.getString("idDepartamento");
+                registro[1]=rs.getString("nombre");
+                registro[2]=rs.getString("superficie");
+                registro[3]=rs.getString("imagen");
+                totalRegistros++;
+                modelo.addRow(registro);
+            }
+            return modelo;
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+            return null;
+        }
+    }
+    
+}
