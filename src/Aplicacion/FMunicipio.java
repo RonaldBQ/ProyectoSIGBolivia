f3053b42be4f510d5978c13827dac5393f17fd97
Ronald Bautista <ronbau21@hotmail.com>
2022-03-31 12:19:24 -0400

    Created Clase FMunicipio
    Se agrega la clase FMunicipio y se agregó el metodo mostrar que devuelve todos los registros de la tabla municipio en un DefaultTableModel

---
+++ src/Aplicacion/FMunicipio.java
@@ -0,0 +1,52 @@
+
+package Aplicacion;
+
+import DBUtils.DBConexion;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * @author Ronald Bautista
+ */
+public class FMunicipio {
+    private DBConexion objBaseDatos = new DBConexion();
+    private Connection cn = objBaseDatos.getConection();
+    private String SQL = "";
+    public int totalRegistros;
+    
+    /**
+     * Método que retorna todos los registros de la tabla Municipios en un modelo DefaultTabkeModel
+     * @return modelo
+     */
+    public DefaultTableModel mostrar(){
+        DefaultTableModel modelo;
+        String [] titulos ={"ID","NOMBRE","POBLACION","provincia"};
+        String [] registro = new String[4];
+        totalRegistros = 0;
+        modelo = new DefaultTableModel(null, titulos);
+        SQL = "SELECT municipio.idMunicipio,municipio.nombre,municipio.poblacion,provincia.nombre AS 'provincia'"
+                + "FROM provincia inner join municipio "
+                + "ON provincia.idProvincia = municipio.idProvincia";
+        try {
+            Statement st = cn.createStatement();
+            ResultSet rs = st.executeQuery(SQL);
+            while(rs.next()){
+                registro[0]=rs.getString("idMunicipio");
+                registro[1]=rs.getString("nombre");
+                registro[2]=rs.getString("poblacion");
+                registro[3]=rs.getString("provincia");
+                totalRegistros++;
+                modelo.addRow(registro);
+            }
+            return modelo;
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex);
+            return null;
+        }
+    }
+    
+}
